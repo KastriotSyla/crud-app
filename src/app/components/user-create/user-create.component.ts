1eb97d858265ef94103f7891c590e5ee8effe204
Kastriot Syla <>
2020-06-16 10:11:58 +0200

    Initial commit

---
+++ src/app/components/user-create/user-create.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+import {FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
+import {ApiService} from 'src/app/_services/api.service';
+
+@Component({
+   selector: 'app-book-create',
+   templateUrl: './user-create.component.html',
+   styleUrls: ['./user-create.component.css']
+})
+export class UserCreateComponent implements OnInit {
+   
+   userForm: FormGroup;
+   id: number;
+   firstName: string = '';
+   LastName: string = '';
+   
+   constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) {
+   }
+   
+   ngOnInit() {
+      this.userForm = this.formBuilder.group({
+         'id': [null, Validators.required],
+         'firstName': [null, Validators.required],
+         'LastName': [null, Validators.required],
+      });
+   }
+   
+   onFormSubmit(form: NgForm) {
+      this.api.postUser(form)
+         .subscribe(res => {
+            let id = res['id'];
+            this.router.navigate(['/user-details', id]);
+         }, (err) => {
+            console.log(err);
+         });
+   }
+}
---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,54 @@
+import {Component, OnInit} from '@angular/core';
+
+import {DataSource} from '@angular/cdk/collections';
+import {Observable} from 'rxjs';
+import {ApiService} from 'src/app/_services/api.service';
+import {Router} from '@angular/router';
+import {AuthenticationService} from 'src/app/_services';
+import {User, Role} from 'src/app/_models/index';
+
+
+@Component({
+   selector: 'app-book',
+   templateUrl: './user.component.html',
+   styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+   currentUser: User;
+   users: any;
+   displayedColumns = ['isbn', 'title', 'author'];
+   dataSource = new UserDataSource(this.api);
+   
+   constructor(private api: ApiService, private router: Router,
+               private authenticationService: AuthenticationService) {
+      this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+   }
+   
+   ngOnInit() {
+      this.api.getUsers()
+         .subscribe(res => {
+            console.log(res);
+            this.users = res;
+         }, err => {
+            console.log(err);
+         });
+   }
+   
+   get isAdmin() {
+      return this.currentUser && this.currentUser.role === Role.Admin;
+   }
+}
+
+export class UserDataSource extends DataSource<any> {
+   constructor(private api: ApiService) {
+      super()
+   }
+   
+   connect() {
+      return this.api.getUsers();
+   }
+   
+   disconnect() {
+   
+   }
+}
